2604ae2d06007f441dd489365318b99b978e654b
N <806642@cognizant.com>
2019-10-23 12:49:49 +0530

    my first commit

---
+++ src/test/java/com/cts/myportalclient1/MyportalClient1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.myportalclient1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyportalClient1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
